22cbd1c0d9e7a9277a22bfbc8746fc948eeda83c
Andrew Swank <adswank@gmail.com>
2025-01-21 12:33:29 -0500

    Deploy site updates

---
+++ _next/static/q_dF2x2YXgiupJ6YiEZ_Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/825-d58c82bc1dfb3a5a.js","static/chunks/pages/index-acac1c6bc61ec987.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
